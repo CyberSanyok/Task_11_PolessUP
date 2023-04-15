e1de53dea037cbf9c9be07c85aaba7278a4d7541
CyberSanyok <sashad92175@gmail.com>
2023-04-15 12:43:53 +0300

    Добавьте файлы проекта.

---
+++ Task_11_PolesUP/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = new int[10] {7,9,8,10,6,5,4,3,2,1 };
+        for (int i = 0; i < array.Length-1; i++) 
+        {
+            for (int j = 0; j < array.Length-1-i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        Console.WriteLine(string.Join(", ",array));
+    }
+}
